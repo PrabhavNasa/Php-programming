4553081c801cac099d0bc10766715310aee38c0f
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2025-04-15 00:03:50 +0530

    Update Q56 sunrise sunset.php

--- Q56 sunrise sunset.php
+++ Q56 sunrise sunset.php
@@ -12,5 +12,5 @@ echo("<br>Sunrise time: ");
 echo(date_sunrise(time(), SUNFUNCS_RET_STRING, 28.7041, 77.1025, 90.5, 5.5));
 echo("<br>Sunset time: ");
 echo(date_sunset(time(), SUNFUNCS_RET_STRING, 28.7041, 77.1025, 90.6, 5.5));
-echo "<br>This program is written by Akeniyamat<br>ERPID-0221BCA153";
+echo "<br>This program is written by Prabhav Nasa<br>ERPID-0221BCA011";
 ?>
